d33148e0a9f5646c40677a82ca53e32ce9d18042
Apple <apple@apples-imac.local>
2019-01-23 20:56:24 +0530

    Initial Commit

---
+++ MusicController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MusicController
+//
+//  Created by Apple on 23/01/19.
+//  Copyright © 2019 Batth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MusicController/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  MusicController
+//
+//  Created by Apple on 23/01/19.
+//  Copyright © 2019 Batth. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
